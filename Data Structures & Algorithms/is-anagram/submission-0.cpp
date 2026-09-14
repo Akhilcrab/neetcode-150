4dea6573b98372b9026c53e243d077ca31d5c69e
Akhil Rawat <akhilrwt2004@gmail.com>
2026-09-14 15:10:22 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+         if( s.length() != t.length()) return false ;
+        
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
+
+         for( int i = 0 ; i < s.length() ; i++){
+             v1[ s[i] - 'a']++;
+             v2[ t[i] - 'a']++;
+         }
+
+        return v1 == v2 ;
+    }
+};
